a7cf930b5d0e21b451fbe4fc6513b1f4ec87c944
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-11-02 23:20:08 +0700

    Applet

--- HTML+CSS+JS+Applet/HelloWorldApplet.java
+++ HTML+CSS+JS+Applet/HelloWorldApplet.java
@@ -3,6 +3,6 @@ import java.awt.*;
 
 public class HelloWorldApplet extends Applet {
    public void paint (Graphics g) {
-      g.drawString ("Hello World", 25, 50);
+      g.drawString ("Kelompom 3 Pemograman WEB", 25, 50);
    }
 }
